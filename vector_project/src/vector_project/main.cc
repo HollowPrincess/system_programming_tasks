803188c2dad12770e8fc89597054c5f3abab0042
Unknown <risahawkeye@mail.ru>
2018-10-12 19:22:45 +0300

    some few fixes in vector + test copy

--- vector_project/src/vector_project/main.cc
+++ vector_project/src/vector_project/main.cc
@@ -6,9 +6,10 @@
 template <typename T>
 class Vector
 {
-protected:
-	T * allocator;
-public:
+  protected:
+	T *allocator;
+
+  public:
 	size_t size;
 	//constructors:
 
@@ -22,15 +23,12 @@ public:
 	//copy constructor
 	Vector(Vector &existingVector)
 	{
-		/*size = existingVector.sizeOfVector();
+		size = existingVector.sizeOfVector();
 		allocator = new T[size];
 		for (size_t counter = 0; counter < size; counter++)
 		{
-			allocator[counter] = std::forward(existingVector.allocator[counter]);
-		}*/
-		Vector<T> tmp();
-		tmp=existingVector;
-		this->swap(tmp);
+			allocator[counter] = std::forward<T>(existingVector.allocator[counter]);
+		};
 	}
 
 	//move constructor
@@ -60,18 +58,19 @@ public:
 
 	//difficult methods:
 	//push_back(T) - add element at the tail
-	void push_back(T&& value)
+	void push_back(T &&value)
 	{
 		T *newAlloc = new T[size + 1];
 		size_t counter = 0;
-		while (counter < size) {
+		while (counter < size)
+		{
 			newAlloc[counter] = std::move(this->allocator[counter]);
 			counter++;
 		}
 		newAlloc[size] = std::move(value);
 		delete[] allocator;
-        allocator = newAlloc;
-		size += 1; 
+		allocator = newAlloc;
+		size += 1;
 	}
 
 	//pop_back() - drop element at the tail
@@ -79,44 +78,53 @@ public:
 	{
 		T *newAlloc = new T[size];
 		size_t counter = 0;
-		while (counter < size) {
+		while (counter < size)
+		{
 			newAlloc[counter] = std::move(this->allocator[counter]);
 			counter++;
 		}
 		delete[] allocator;
-        allocator = newAlloc;		 
+		allocator = newAlloc;
 		size -= 1;
 	}
 
 	//erase() - delete elements //look cpp
 	//erase one element at position = num_of_pos
 	void erase(size_t num_of_pos)
-	{		
-		//move the tail on one position and pop_back		
-		if (this->sizeOfVector() > num_of_pos) {
-			for (size_t counter = num_of_pos; counter<this->sizeOfVector(); counter++) {
+	{
+		//move the tail on one position and pop_back
+		if (this->sizeOfVector() > num_of_pos)
+		{
+			for (size_t counter = num_of_pos; counter < this->sizeOfVector(); counter++)
+			{
 				allocator[counter] = std::forward<T>(allocator[counter + 1]);
 			}
 			this->pop_back();
 		}
-		else if(this->sizeOfVector() == num_of_pos)	this->pop_back();	
+		else if (this->sizeOfVector() == num_of_pos)
+			this->pop_back();
 	}
 
 	void erase(size_t first_num, size_t last_num)
 	{
-		if (first_num>last_num) {
+		if (first_num > last_num)
+		{
 			first_num += last_num;
 			last_num = first_num - last_num;
 			first_num = first_num - last_num;
-		};		
+		};
 		//move the tail on (last_num-first_num) position and pop_back
-		if (this->sizeOfVector() >= last_num) {
-			for (size_t counter = first_num; counter<this->sizeOfVector(); counter++) {
-				if (size>=counter+(last_num - first_num)+1){
-					allocator[counter] = std::forward<T>(allocator[counter + (last_num - first_num)+1]);
+		if (this->sizeOfVector() >= last_num)
+		{
+			for (size_t counter = first_num; counter < this->sizeOfVector(); counter++)
+			{
+				if (size >= counter + (last_num - first_num) + 1)
+				{
+					allocator[counter] = std::forward<T>(allocator[counter + (last_num - first_num) + 1]);
 				};
 			};
-			for (size_t counter = first_num; counter<size; counter++) {
+			for (size_t counter = first_num; counter < size; counter++)
+			{
 				this->pop_back();
 			};
 		};
@@ -124,34 +132,27 @@ public:
 	//swap() vectors
 	void swap(Vector<T> &rightVector)
 	{
-		/*Vector<T> tmp(*this);
-		*this = std::move(rightVector);
-		rightVector = std::move(tmp);*/
-		T *tmpAlloc=allocator;
+		T *tmpAlloc = allocator;
 		size_t tmpSize = size;
 
-		allocator=rightVector.allocator;
-		size=rightVector.size;
+		allocator = rightVector.allocator;
+		size = rightVector.size;
 
-        rightVector.allocator = tmpAlloc;
-        rightVector.size = tmpSize;
+		rightVector.allocator = tmpAlloc;
+		rightVector.size = tmpSize;
 	}
 
 	//operators:
 	//operator=
 	Vector &operator=(const Vector &&rightVector)
 	{
-		size = rightVector.sizeOfVector();
-		allocator = new T[size];
-		for (size_t counter = 0; counter < size; counter++)
+		if (this != &rightVector)
 		{
-			allocator[counter] = std::forward(rightVector.allocator[counter]);
+			delete[] allocator;
+			Vector<T> tmp(rightVector);
+			this->swap(tmp);
 		};
-		
-		//Vector<T> tmp(rightVector);
-		//this->swap(tmp);
 		return *this;
-		
 	}
 
 	Vector &operator=(Vector &&rightVector)
@@ -160,7 +161,8 @@ public:
 		return *this;
 	}
 
-	T &operator[](const size_t pos) {
+	T &operator[](const size_t pos)
+	{
 		return allocator[pos];
 	}
 
@@ -191,8 +193,8 @@ int main()
 	std::ofstream ofs5("ofs5.txt", std::ofstream::out);
 	std::ofstream ofs6("ofs6.txt", std::ofstream::out);
 
-	Vector<std::ofstream> myVec; 
-	
+	Vector<std::ofstream> myVec;
+
 	myVec.push_back(std::move(ofs1));
 	myVec.push_back(std::move(ofs2));
 	myVec.push_back(std::move(ofs3));
@@ -209,23 +211,27 @@ int main()
 	myVec[2] << 3;
 	myVec[3] << 4;
 
-	//oper= test:
+	//oper= test
 	Vector<std::ofstream> anotherVec1;
 	Vector<std::ofstream> anotherVec2;
+
 	anotherVec1.push_back(std::move(ofs5));
 	anotherVec2.push_back(std::move(ofs6));
+	Vector<std::ofstream> anotherVec3(anotherVec1);
 
-	anotherVec1=std::move(anotherVec2);
+	anotherVec1 = std::move(anotherVec2);
 	anotherVec1[0] << "must be ofs6.txt file";
-
+	anotherVec3[0] << "\nHello! I am a copy constructor in ofs5.txt";
 
 	//pop_back test
-	myLog << "pop_back test"<<"\n";
+	myLog << "pop_back test"
+		  << "\n";
 	myLog << "size: ";
 	myLog << myVec.sizeOfVector();
-	myLog << "\n";	
+	myLog << "\n";
 
-	myVec[3] << "\n"<<"it was pop_back, so it contains only one number";
+	myVec[3] << "\n"
+			 << "it was pop_back, so it contains only one number";
 	myVec.pop_back();
 
 	myLog << "size: ";
@@ -241,24 +247,26 @@ int main()
 	myVec[2] << "==3";
 	myVec[2] << "\n";
 
-	*(myVec.end()-1)<<" & !=4"<<"\n";
+	*(myVec.end() - 1) << " & !=4"
+					   << "\n";
 
 	//erase test
-	myLog << "erase test"<<"\n";
+	myLog << "erase test"
+		  << "\n";
 	myLog << "size: ";
 	myLog << myVec.sizeOfVector();
-	myLog << "\n";	
+	myLog << "\n";
 
-	myVec[0]<<" it was erased";
-	myVec[1]<<" it was erased";
-	myVec.erase(0,1);
+	myVec[0] << " it was erased";
+	myVec[1] << " it was erased";
+	myVec.erase(0, 1);
 
 	myLog << "size: ";
 	myLog << myVec.sizeOfVector();
 	myLog << "\n";
 	//myVec[0] << "==1";
 	//myVec[1] << "!=2 & ==3";
-	myVec[0]<<"==3";
+	myVec[0] << "==3";
 
 	return 0;
 }
