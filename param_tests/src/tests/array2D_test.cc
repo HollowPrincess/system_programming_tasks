28e5ec00c7b5a6a16c452b0ea91497dd09535131
Unknown <risahawkeye@mail.ru>
2018-10-17 17:51:02 +0300

    error in tests

--- param_tests/src/tests/array2D_test.cc
+++ param_tests/src/tests/array2D_test.cc
@@ -1,37 +1,61 @@
 #include <../param_tests/MyArray2D.cc>
 #include <gtest/gtest.h>
 
-//подход AAA (arrange, act, assert)
 struct array2D_param_for_arithmetical_operators {
   MyArray2D<double> inputLeft;
   MyArray2D<double> inputRight;
-  MyArray2D<double> output;
+  double *output;
+};
+
+struct array2D_param_for_logical_operators {
+  MyArray2D<double> inputLeft;
+  MyArray2D<double> inputRight;
+  bool *output;
 };
 
 class array2D_test : public ::testing::TestWithParam<
                          array2D_param_for_arithmetical_operators> {};
 
-double Array1[4] = {0.1, 0, -0.3, -1};
-double Array2[4] = {0.1, 0, -0.3, 1};
-double Array3[4] = {0.2, 0, -0.6, 0};
-double Array4[2] = {0.1, 0};
-MyArray2D<double> Left = MyArray2D<double>(2, 2, Array1);
-MyArray2D<double> inputRightTrueSize{2, 2, Array2};
-MyArray2D<double> outputSum{2, 2, Array3};
+double arrayLeft[4] = {0.1, 0, -0.3, -1};
+double arrayRight[4] = {0.1, 1, -0.3, 1};
 
-MyArray2D<double> inputRightFalseSize{2, 1, Array4};
+double arraySum[4] = {0.2, 1, -0.6, 0};
+double arrayProduct[4] = {0.01, 0, 0.09, -1};
+double arraySubtraction[4] = {0, -1, 0, -2};
+double arrayDivision[4] = {1, 0, 1, -1};
+double arrayMod[4] = {0, 0 % 1, 0, -1 % 1};
 
-INSTANTIATE_TEST_CASE_P(
-    arithmetical_operators_test, array2D_test,
-    ::testing::Values(array2D_param_for_arithmetical_operators{
-        Left, inputRightTrueSize, outputSum}));
+double arrayWithAnotherSize[2] = {0.1, 0};
+
+MyArray2D<double> left = MyArray2D<double>(2, 2, arrayLeft);
+MyArray2D<double> inputRightTrueSize{2, 2, arrayRight};
+
+MyArray2D<double> inputRightFalseSize{2, 1, arrayWithAnotherSize};
 
 TEST_P(array2D_test, sum_operator_test) {
   const array2D_param_for_arithmetical_operators &param = GetParam();
   MyArray2D<double> result = param.inputLeft + param.inputRight;
 
-  EXPECT_EQ(param.output(0, 0), result(0, 0));
-  EXPECT_EQ(param.output(0, 1), result(0, 1));
-  EXPECT_EQ(param.output(1, 0), result(1, 0));
-  EXPECT_EQ(param.output(1, 1), result(1, 1));
+  EXPECT_EQ(param.output[0], result(0, 0));
+  EXPECT_EQ(param.output[1], result(0, 1));
+  EXPECT_EQ(param.output[2], result(1, 0));
+  EXPECT_EQ(param.output[3], result(1, 1));
 }
+INSTANTIATE_TEST_CASE_P(
+    sum, array2D_test,
+    ::testing::Values(array2D_param_for_arithmetical_operators{
+        left, inputRightTrueSize, arraySum}));
+
+TEST_P(array2D_test, prod_operator_test) {
+  const array2D_param_for_arithmetical_operators &param = GetParam();
+  MyArray2D<double> result = param.inputLeft * param.inputRight;
+
+  EXPECT_EQ(param.output[0], result(0, 0));
+  EXPECT_EQ(param.output[1], result(0, 1));
+  EXPECT_EQ(param.output[2], result(1, 0));
+  EXPECT_EQ(param.output[3], result(1, 1));
+}
+INSTANTIATE_TEST_CASE_P(
+    prod, array2D_test,
+    ::testing::Values(array2D_param_for_arithmetical_operators{
+        left, inputRightTrueSize, arrayProduct}));
