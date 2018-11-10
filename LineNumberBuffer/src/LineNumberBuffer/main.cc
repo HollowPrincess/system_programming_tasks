7344067ba3664291dba30ca9cefd222ed9062273
Unknown <risahawkeye@mail.ru>
2018-11-10 18:23:20 +0300

    fixes

--- LineNumberBuffer/src/LineNumberBuffer/main.cc
+++ LineNumberBuffer/src/LineNumberBuffer/main.cc
@@ -1,12 +1,12 @@
-#include <../LineNumberBuffer/LineNumberBuffer.cc>
+#include "../LineNumberBuffer/LineNumberBuffer.cc"
 #include <iostream>
 #include <string>
 
 int main() {
   std::streambuf *oldbuf = std::cout.rdbuf();
-  LineNumberBuffer mybuf(oldbuf);
-  std::cout.rdbuf(&mybuf);
-
-  std::cout.rdbuf(oldbuf); // recovery of old buf
+  LineNumberBuffer *mybuf = new LineNumberBuffer(oldbuf);
+  std::cout.rdbuf(mybuf);
+  std::cout << std::cin.rdbuf();
+  std::cout.rdbuf(oldbuf);
   return 0;
 }
