74b9ee725300706c07f3b4ac3d1c8ce0bcf0596b
Unknown <risahawkeye@mail.ru>
2018-11-22 21:16:02 +0300

    update

--- parallel_sorting_algorithm/src/parallel_sorting_algorithm/parallel_sort.cc
+++ parallel_sorting_algorithm/src/parallel_sorting_algorithm/parallel_sort.cc
@@ -1,75 +1,78 @@
-#include <vector>
-#include <thread>
-#include <mutex>
-
 #include <fstream>
-std::ofstream myLog("myLog.txt", std::ofstream::out);
+#include <math.h>
+#include <thread>
+#include <vector>
 
 unsigned int n = std::thread::hardware_concurrency();
-myLog<<n;
 std::vector<std::thread> threads{n};
-//std::mutex mutex;
 
-template<typename T, typename Iter>
-void parallel_sorting(Iter beginElement, Iter endElement){
-        //mutex.lock();
-        // end element is element after last element in vector
-        // if all elements not divised
+template <typename T, typename Iter>
+void parallel_sorting(Iter beginElement, Iter endElement) {
+  // end element is element after last element in vector
+  // if all elements not divised
 
-        if (beginElement + 1 < endElement) {
-                // division
-                typename std::vector<T>::iterator leftArrayCurrentElement = beginElement;
-                typename std::vector<T>::iterator rightArrayCurrentElement = beginElement;
-                advance(rightArrayCurrentElement,
-                        round((endElement - 1 - beginElement) / 2) + 1);
+  if (beginElement + 1 < endElement) {
+    // division
+    typename std::vector<T>::iterator leftArrayCurrentElement = beginElement;
+    typename std::vector<T>::iterator rightArrayCurrentElement = beginElement;
+    advance(rightArrayCurrentElement,
+            round((endElement - 1 - beginElement) / 2) + 1);
 
-                //std::lock_guard<std::mutex> locked(mutex);
-                bool isOneThreadBusy=false;
-                for(auto& thr : threads) {
-                        if(!isOneThreadBusy) {
-                                if(thr.joinable()) {
-                                        thr=std::thread{parallel_sorting<T>, beginElement, rightArrayCurrentElement};
-                                        isOneThreadBusy=true;
-                                };
-                        }else{
-                                if(thr.joinable()) {
-                                        thr=std::thread{parallel_sorting<T>, rightArrayCurrentElement, endElement};
-                                        thr.join();
-                                        break;
-                                };
-                        };
-                };
-                parallel_sorting(beginElement, rightArrayCurrentElement);
-                parallel_sorting(rightArrayCurrentElement, endElement);
-
-                // merge sorted arrays
-                std::vector<T> tmpVector(endElement - beginElement); // empty
-
-                for (typename std::vector<T>::iterator resultVectorCurrentElement =
-                             tmpVector.begin();
-                     resultVectorCurrentElement != tmpVector.end();
-                     resultVectorCurrentElement++) {
-                        if ((rightArrayCurrentElement == endElement) ||
-                            (*leftArrayCurrentElement < *rightArrayCurrentElement)) {
-                                *resultVectorCurrentElement = *leftArrayCurrentElement;
-                                leftArrayCurrentElement++;
-                        } else {
-                                *resultVectorCurrentElement = *rightArrayCurrentElement;
-                                rightArrayCurrentElement++;
-                        };
-                };
-
-                typename std::vector<T>::iterator resultVectorCurrentElement =
-                        tmpVector.begin();
-                for (typename std::vector<T>::iterator iter = beginElement;
-                     iter != endElement; iter++, resultVectorCurrentElement++) {
-                        *iter = *resultVectorCurrentElement;
-                };
-                /*
-                   for (typename std::vector<T>::iterator iter = beginElement;
-                     iter != endElement; iter++) {
-                   };*/
+    bool isOneThreadBusy = false;
+    for (auto &thr : threads) {
+      if (!isOneThreadBusy) {
+        if (thr.joinable()) {
+          thr = std::thread{parallel_sorting<T, Iter>, beginElement,
+                            rightArrayCurrentElement};
+          isOneThreadBusy = true;
         };
+      } else {
+        if (thr.joinable()) {
+          thr = std::thread{parallel_sorting<T, Iter>, rightArrayCurrentElement,
+                            endElement};
+          break;
+        };
+      };
+    };
+    for (auto &thr : threads) {
+      if (thr.joinable()) {
+        thr.join();
+      };
+    };
+    parallel_sorting<T>(beginElement, rightArrayCurrentElement);
+    parallel_sorting<T>(rightArrayCurrentElement, endElement);
 
-        //mutex.unlock();
+    // merge sorted arrays
+    std::vector<T> tmpVector(endElement - beginElement); // empty
+
+    for (typename std::vector<T>::iterator resultVectorCurrentElement =
+             tmpVector.begin();
+         resultVectorCurrentElement != tmpVector.end();
+         resultVectorCurrentElement++) {
+      if ((rightArrayCurrentElement == endElement) ||
+          (*leftArrayCurrentElement < *rightArrayCurrentElement)) {
+        *resultVectorCurrentElement = *leftArrayCurrentElement;
+        leftArrayCurrentElement++;
+      } else {
+        *resultVectorCurrentElement = *rightArrayCurrentElement;
+        rightArrayCurrentElement++;
+      };
+    };
+
+    typename std::vector<T>::iterator resultVectorCurrentElement =
+        tmpVector.begin();
+    for (typename std::vector<T>::iterator iter = beginElement;
+         iter != endElement; iter++, resultVectorCurrentElement++) {
+      *iter = *resultVectorCurrentElement;
+    };
+  };
+}
+
+int main() {
+  unsigned int k = std::thread::hardware_concurrency();
+  std::ofstream myLog("myLog.txt", std::ofstream::out);
+  myLog << k;
+  std::vector<int> vec{5, 3, 4, 2};
+  parallel_sorting<int>(vec.begin(), vec.end());
+  return 0;
 }
