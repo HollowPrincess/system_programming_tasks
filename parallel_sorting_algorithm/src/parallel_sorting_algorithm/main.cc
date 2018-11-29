d644e42dbd281a7b9152888c87ca650329e7ce67
Unknown <risahawkeye@mail.ru>
2018-11-29 18:19:49 +0300

    fix mistake with time interval

--- parallel_sorting_algorithm/src/parallel_sorting_algorithm/main.cc
+++ parallel_sorting_algorithm/src/parallel_sorting_algorithm/main.cc
@@ -4,7 +4,10 @@
 #include <thread>
 #include <vector>
 
-// using seconds = std::chrono::duration<long, long>;
+// using myTime = std::chrono::seconds;
+// using myTime = std::chrono::milliseconds;
+using myTime = std::chrono::microseconds;
+// using myTime = std::chrono::nanoseconds;
 
 void lineOut(std::ofstream &myLog) {
   for (int counter = 0; counter < 40; counter++) {
@@ -18,31 +21,40 @@ int main() {
   std::ofstream myLog("myLog.txt", std::ofstream::out);
   myLog << "Number Of Kernels: " << k << "\n";
 
-  std::vector<std::chrono::microseconds> vectorOfIntervals;
+  std::vector<myTime> vectorOfIntervals;
   std::vector<int> vecForSort = {5, 3, 4, 2};
   int maxSizeOfVec = vecForSort.size();
   std::vector<int> vecCopy;
+  std::chrono::time_point<std::chrono::system_clock> start;
+  std::chrono::system_clock::duration tseStart;
+  std::chrono::time_point<std::chrono::system_clock> end;
+  std::chrono::system_clock::duration tseEnd;
+  myTime s;
   for (int counter = 0; counter < maxSizeOfVec; counter++) {
     vecCopy = vecForSort;
     vecCopy.pop_back();
-    auto now = std::chrono::system_clock::now();
+
+    start = std::chrono::system_clock::now();
     parallel_sorting<int>(vecForSort.begin(), vecForSort.end());
-    std::chrono::system_clock::duration tse = now.time_since_epoch();
-    std::chrono::microseconds s =
-        std::chrono::duration_cast<std::chrono::microseconds>(tse);
+    end = std::chrono::system_clock::now();
+
+    tseStart = start.time_since_epoch();
+    tseEnd = end.time_since_epoch();
+
+    s = std::chrono::duration_cast<myTime>(tseEnd - tseStart);
     vectorOfIntervals.push_back(s);
     vecForSort = vecCopy;
   };
 
   myLog << "Time Table: \n";
   lineOut(myLog);
-  myLog << "| VecSize: | Duration:       |\n";
-  std::chrono::microseconds elem;
+  myLog << "| VecSize: | Duration:       \n";
+  myTime elem;
 
   for (int counter = 1; counter <= maxSizeOfVec; counter++) {
     lineOut(myLog);
     elem = vectorOfIntervals.back();
-    myLog << "| " << counter << "        | " << elem.count() << "|\n ";
+    myLog << "| " << counter << "        | " << elem.count() << "\n ";
     vectorOfIntervals.pop_back();
   };
   lineOut(myLog);
